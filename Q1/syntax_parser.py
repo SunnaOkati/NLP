df3fb6e9668217c8eababe37dd3c798717219a46
SunnOkati <ranjth2raj@gmail.com>
2021-10-24 00:29:12 +1100

    Calculates Conditional Probability and writes it to desired file "q1.json".

--- Q1/syntax_parser.py
+++ Q1/syntax_parser.py
@@ -47,15 +47,66 @@ class RuleWriter(object):
         """
         json.dump(self.rules, open(filename, "w"))
 
-# load the parsed sentences
-psents = json.load(open("parsed_sents_list.json", "r"))
-#psents = [['A', ['B', ['C', 'blue']], ['B', 'cat']]] # test case
-
-# print a few parsed sentences
-# NOTE: you can remove this if you like
-for sent in psents[:10]:
-    print(sent)
 
 # TODO: estimate the conditional probabilities of the rules in the grammar
+def form_relation(parent, children):
+    if (isinstance(children, list) == False):
+        if parent in rel_dict:
+            rel_dict[parent].append([children])
+        else:
+            rel_dict[parent] = [[children]]
 
-# TODO: write the rules to the correct output file using the write_rules method
+    else:
+        rhs = []
+        for child in children:
+            rhs.append(child[0])
+            if(len(child)==2 and isinstance(child[1], list) == False):
+                form_relation(child[0], str(child[1]))
+            else:
+                form_relation(child[0], child[1:])
+
+        if parent in rel_dict:
+            #print(rhs)
+            rel_dict[parent].append(rhs)
+        else:
+            #print(rhs)
+            rel_dict[parent] = [rhs]
+
+def calculate_prob(rel_dict, rule_writer):
+    for lhs in rel_dict.keys():
+        rhs = rel_dict[lhs]
+        # Initialize dict
+        prob_map = {}
+
+        # Use Append through Iteration
+        for e in rhs:
+            prob_map.setdefault(tuple(e), list()).append(1)
+        for key, val in prob_map.items():
+            prob_map[key] = sum(val)/len(rhs)
+
+        for rhs, prob in prob_map.items():
+            rule_writer.add_rule(lhs, rhs, prob)
+            #print("Alpha: " + key + " Beta: " + e[0] + " Prob: " + str(e[1]))
+
+
+def main():
+    # load the parsed sentences
+    psents = json.load(open("parsed_sents_list.json", "r"))
+    #psents = [['A', ['B', ['C', 'blue']], ['B', 'cat']]] # test case
+
+    # print a few parsed sentences
+    # NOTE: you can remove this if you like
+    for sent in psents:
+        print(sent)
+    lhs = psents[0][0]
+    form_relation(lhs, psents[0][1:])
+    print(rel_dict)
+
+    # TODO: write the rules to the correct output file using the write_rules method
+    rule_writer = RuleWriter()
+    calculate_prob(rel_dict, rule_writer)
+    rule_writer.write_rules()
+
+if __name__ == "__main__":
+    rel_dict = {}
+    main()
