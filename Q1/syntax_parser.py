26a3ff55d552974b0d269fbc88754d9d37b22a52
SunnOkati <ranjth2raj@gmail.com>
2021-10-23 14:31:29 +1100

    Initial Commit

---
+++ Q1/syntax_parser.py
@@ -0,0 +1,61 @@
+import json
+import numpy as np
+from collections import defaultdict
+
+class RuleWriter(object):
+    """
+    This class is for writing rules in a format 
+    the judging software can read
+    Usage might look like this:
+
+    rule_writer = RuleWriter()
+    for lhs, rhs, prob in out_rules:
+        rule_writer.add_rule(lhs, rhs, prob)
+    rule_writer.write_rules()
+    """
+
+    def __init__(self):
+        self.rules = []
+
+    def add_rule(self, lhs, rhs, prob):
+        """Add a rule to the list of rules
+        Does some checking to make sure you are using the correct format.
+
+        Args:
+            lhs (str): The left hand side of the rule as a string
+            rhs (Iterable(str)): The right hand side of the rule. 
+                Accepts an iterable (such as a list or tuple) of strings.
+            prob (float): The conditional probability of the rule.
+        """
+        assert isinstance(lhs, str)
+        assert isinstance(rhs, list) or isinstance(rhs, tuple)
+        assert not isinstance(rhs, str)
+        nrhs = []
+        for cl in rhs:
+            assert isinstance(cl, str)
+            nrhs.append(cl)
+        assert isinstance(prob, float)
+
+        self.rules.append((lhs, nrhs, prob))
+
+        
+    def write_rules(self, filename="q1.json"):
+        """Write the rules to an output file.
+
+        Args:
+            filename (str, optional): Where to output the rules. Defaults to "q1.json".
+        """
+        json.dump(self.rules, open(filename, "w"))
+
+# load the parsed sentences
+psents = json.load(open("parsed_sents_list.json", "r"))
+#psents = [['A', ['B', ['C', 'blue']], ['B', 'cat']]] # test case
+
+# print a few parsed sentences
+# NOTE: you can remove this if you like
+for sent in psents[:10]:
+    print(sent)
+
+# TODO: estimate the conditional probabilities of the rules in the grammar
+
+# TODO: write the rules to the correct output file using the write_rules method
